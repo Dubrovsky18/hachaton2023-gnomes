f659de76d74d1778ebe9f7b384093ad2b48456d9
Dubrovsky18 <dubrovin.02@list.ru>
2023-10-21 02:49:12 +0300

    Models in dev

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -9,19 +9,22 @@ import (
 // Column names at database like "column_name"
 // SecondName: "second_name"
 type User struct {
-	Name       string    `gorm:"column:name" json:"name"`
-	SecondName string    `gorm:"column:second_name" json:"second_name"`
-	Phone      string    `gorm:"column:phone" json:"phone"`
-	Email      string    `gorm:"column:email" json:"email"`
-	Date       time.Time `gorm:"column:date" json:"date"`
-	Password   string    `gorm:"column:password" json:"password"`
+	Name       string `gorm:"column:name" json:"name"`
+	SecondName string `gorm:"column:second_name" json:"second_name"`
+	Phone      string `gorm:"column:phone" json:"phone"`
+	Email      string `gorm:"column:email" json:"email"`
+	Password   string `gorm:"column:password" json:"password"`
+}
+
+type Hidden struct {
+	User User `gorm:"embedded;"`
 }
 
 type Teacher struct {
 	gorm.Model
-	User        User   `gorm:"embedded;"`
-	Hours       int    `gorm:"column:hours" json:"hours"`
-	WeekendsDay string `gorm:"column:weekends_day" json:"weekends_day"`
+	User        User `gorm:"embedded;"`
+	Hours       int  `gorm:"column:hours" json:"hours"`
+	WeekendsDay int  `gorm:"column:weekends_day" json:"weekends_day"`
 }
 
 type Admin struct {
@@ -45,28 +48,23 @@ type Student struct {
 
 type Subject struct {
 	gorm.Model
-	Discipline string  `gorm:"column:discipline" json:"discipline"`
-	TeacherID  Teacher `gorm:"foreignkey:teacher_id;association_foreignkey:id"`
-}
-
-type Lesson struct {
-	gorm.Model
-	SubjectID    Subject `gorm:"foreignkey:subject_id;association_foreignkey:id"`
-	GroupID      Group   `gorm:"foreignkey:group_id;association_foreignkey:id"`
-	HoursSemestr int     `gorm:"column:hours_semestr" json:"hours_semestr"`
-	Type         string  `gorm:"column:type" json:"type"`
+	Discipline  string  `gorm:"column:discipline" json:"discipline"`
+	TeacherID   Teacher `gorm:"foreignkey:teacher_id;association_foreignkey:id"`
+	LessonHours int     `gorm:"column:lesson_hours" json:"lesson_hours"`
+	Type        string  `gorm:"column:type" json:"type"`
 }
 
 type Audience struct {
 	gorm.Model
-	Status   bool `gorm:"column:status" json:"status"`
-	Capacity int  `gorm:"column:capacity" json:"capacity"`
+	Name string `gorm:"column:name" json:"name"`
+	Type string `gorm:"column:type" json:"type"`
 }
 
 type Schedule struct {
 	gorm.Model
-	LessonID   Lesson    `gorm:"foreignkey:lesson_id;association_foreignkey:id"`
+	SubjectID  Subject   `gorm:"foreignkey:subject_id;association_foreignkey:id"`
 	AudienceID Audience  `gorm:"foreignkey:audience_id;association_foreignkey:id"`
+	GroupID    Group     `gorm:"foreignkey:group_id;association_foreignkey:id"`
 	Date       time.Time `gorm:"column:date" json:"date"`
 	Time       time.Time `gorm:"column:time" json:"time"`
 }
